a9f52ee54eee675542e61906bb64fc99462bb077
id0ba <72280374+ID0BA@users.noreply.github.com>
2024-07-24 14:35:40 +0100

    Update my_script.sh

--- my_script.sh
+++ my_script.sh
@@ -2,6 +2,6 @@
 
 echo "running automation..." > "./output_log.txt"
 
-echo "" > ./output_log.txt
-echo "checking uptime..." > "./output_log.txt
+echo "" >> ./output_log.txt
+echo "checking uptime..." >> "./output_log.txt"
 echo $(uptime) >> ./output_log.txt
